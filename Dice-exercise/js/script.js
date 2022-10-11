a528a0d6a51d69d628451be82089e3fc7ba1c59c
BetterCallAle <dap22091998@gmail.com>
2022-10-11 16:50:11 +0200

    created an empty array and run the cicle for twice for add in the array some random number

--- Dice-exercise/js/script.js
+++ Dice-exercise/js/script.js
@@ -1,1 +1,13 @@
-alert("prova")
+//created an empty array
+const randomNumber = []
+
+//run the random math twice and export it to the array
+for(i = 1; i <= 2; i++){
+    const randomNumberGenerator = Math.floor(Math.random() * (7 - 1) + 1)
+    randomNumber.push(randomNumberGenerator)
+}
+
+const myNumber = randomNumber[0];
+const userNumber = randomNumber[1];
+
+console.log(myNumber, userNumber);
