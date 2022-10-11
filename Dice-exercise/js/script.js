38d2ea8d8e418479ef0900e869a433abf7823cd8
BetterCallAle <dap22091998@gmail.com>
2022-10-11 17:27:33 +0200

    added a condition

--- Dice-exercise/js/script.js
+++ Dice-exercise/js/script.js
@@ -1,13 +1,20 @@
 //created an empty array
 const randomNumber = []
-
 //run the random math twice and export it to the array
-for(i = 1; i <= 2; i++){
+
+for(let i = 1; i <= 2; i++){
     const randomNumberGenerator = Math.floor(Math.random() * (7 - 1) + 1)
     randomNumber.push(randomNumberGenerator)
 }
 
-const myNumber = randomNumber[0];
+const  myNumber = randomNumber[0];
 const userNumber = randomNumber[1];
-
 console.log(myNumber, userNumber);
+
+if(myNumber > userNumber){
+    console.log("Ho vinto io");
+} else if (myNumber < userNumber){
+    console.log("Hai vinto tu");
+} else if (myNumber === userNumber){
+    console.log("Abbiamo pareggiato");
+}
