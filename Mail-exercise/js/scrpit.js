33a43ed29077351031db508ad6505277a86849f2
BetterCallAle <dap22091998@gmail.com>
2022-10-11 15:49:22 +0200

    the page is now interactive + give some (little) style

--- Mail-exercise/js/scrpit.js
+++ Mail-exercise/js/scrpit.js
@@ -1,23 +1,54 @@
 const mailList = ["pippofranco@gmail.com", "gabibbotuttorosso@outlook.com", "divinootelma@boolean.it", "tedmosby@yahoo.com"];
-console.log(mailList);
 
-//ask the user for his email
-const userMail = prompt("Inserisci la tua mail");
-console.log(userMail);
 
-//Check if the user mail is in the mailList
-let registeredMail = false;
+//extract from DOM
+const userMail = document.getElementById("user-email");
 
-for (i = 0; i < mailList.length; i++){
-    let thisMail = mailList[i];
+const submitBtn = document.getElementById("submit");
 
-    if(userMail === thisMail){
-        registeredMail = true;
+const cancelBtn = document.getElementById("cancel");
+
+const sectionHeading = document.querySelector(".section-heading")
+
+
+//on click export data
+submitBtn.addEventListener("click", function() {
+    //cleaning the sectionHeading
+    sectionHeading.innerHTML = ""
+    console.log(sectionHeading.innerHTML);
+
+    const userMailValue = userMail.value;
+    //Check if the user mail is in the mailList
+    let registeredMail = false;
+
+    for (i = 0; i < mailList.length; i++){
+        let thisMail = mailList[i];
+
+        if(userMailValue === thisMail){
+            registeredMail = true;
+        }
     }
-}
 
-if(registeredMail){
-    alert("La tua mail è nel nostro database, puoi entrare.")
-} else {
-    alert("La tua mail non è nel nostro database, iscriviti prima di poter continuare")
-}
+    //create an element
+    const elementCreated = document.createElement("h2");
+
+    if(registeredMail){
+        elementCreated.innerHTML = `Hai effettuato l'accesso con l'email: ${userMailValue}`
+    } else {
+        elementCreated.innerHTML = `La tua email ${userMailValue} non è registrata. Effettua l'iscrizione prima di continuare`
+    }
+
+    sectionHeading.append(elementCreated)
+
+    //cleaning the data
+    userMail.value = ""
+})
+
+//cancel button
+cancelBtn.addEventListener("click", function(){
+    userMail.value = ""
+    sectionHeading.innerHTML = ""
+})
+
+
+
