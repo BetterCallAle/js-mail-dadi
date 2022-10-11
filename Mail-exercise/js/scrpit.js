1da7daa55176bbcc0aa89541a3a74d76f0ccf01e
BetterCallAle <dap22091998@gmail.com>
2022-10-11 15:17:56 +0200

    created an output condition

--- Mail-exercise/js/scrpit.js
+++ Mail-exercise/js/scrpit.js
@@ -9,6 +9,15 @@ console.log(userMail);
 let registeredMail = false;
 
 for (i = 0; i < mailList.length; i++){
-    let thisMail = mailList [i];
-    console.log(thisMail);
+    let thisMail = mailList[i];
+
+    if(userMail === thisMail){
+        registeredMail = true;
+    }
+}
+
+if(registeredMail){
+    alert("La tua mail è nel nostro database, puoi entrare.")
+} else {
+    alert("La tua mail non è nel nostro database, iscriviti prima di poter continuare")
 }
