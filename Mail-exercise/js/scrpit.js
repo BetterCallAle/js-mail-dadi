4f5baeb8a8ad01fd0e07e38e1d86447d0f5a8bb2
BetterCallAle <dap22091998@gmail.com>
2022-10-11 15:12:11 +0200

    created a for cicle

--- Mail-exercise/js/scrpit.js
+++ Mail-exercise/js/scrpit.js
@@ -1,2 +1,14 @@
 const mailList = ["pippofranco@gmail.com", "gabibbotuttorosso@outlook.com", "divinootelma@boolean.it", "tedmosby@yahoo.com"];
 console.log(mailList);
+
+//ask the user for his email
+const userMail = prompt("Inserisci la tua mail");
+console.log(userMail);
+
+//Check if the user mail is in the mailList
+let registeredMail = false;
+
+for (i = 0; i < mailList.length; i++){
+    let thisMail = mailList [i];
+    console.log(thisMail);
+}
