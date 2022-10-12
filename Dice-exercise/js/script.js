df7978648f11eec36b36b534188c1dc3f645a7fa
BetterCallAle <dap22091998@gmail.com>
2022-10-12 10:01:31 +0200

    correction during lesson

--- Dice-exercise/js/script.js
+++ Dice-exercise/js/script.js
@@ -1,20 +1,13 @@
-//created an empty array
-const randomNumber = []
-//run the random math twice and export it to the array
+const myNumber = Math.floor(Math.random() * (7 - 1) + 1);
+const yourNumber = Math.floor(Math.random() * (7 - 1) + 1);
+console.log(myNumber, yourNumber); 
 
-for(let i = 1; i <= 2; i++){
-    const randomNumberGenerator = Math.floor(Math.random() * (7 - 1) + 1)
-    randomNumber.push(randomNumberGenerator)
-}
+if (yourNumber > myNumber){
+   console.log("hai vinto tu");
+} else if (yourNumber < myNumber){
+   console.log("Ho vinto io");
+} else if (yourNumber === myNumber){
+   console.log("Abbiamo pareggiato");
+} 
 
-const  myNumber = randomNumber[0];
-const userNumber = randomNumber[1];
-console.log(myNumber, userNumber);
 
-if(myNumber > userNumber){
-    console.log("Ho vinto io");
-} else if (myNumber < userNumber){
-    console.log("Hai vinto tu");
-} else if (myNumber === userNumber){
-    console.log("Abbiamo pareggiato");
-}
